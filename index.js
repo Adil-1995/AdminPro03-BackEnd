4ecc0aa10c7805d2ba9af20f4f1a27897d0e60dd
Adil-1995 <achairadil@gmail.com>
2021-08-08 12:41:15 +0200

    reload page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const { dbConnection } = require('./database/config');
 
@@ -28,6 +29,11 @@ app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/upload', require('./routes/uploads') );
+
+//lo ultimo
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+});
 
 
 
